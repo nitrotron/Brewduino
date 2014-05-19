772309ee9b6cf1e3759c50a84bd28bad7a786f82
nitrotron <toddmetcalf@gmail.com>
2014-05-19 12:26:03 -0400

    Added back missing pages

---
+++ Brewduino/Pages/WebForm2.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BrewduinoCatalogLib;
+using System.IO.Ports;
+
+namespace Brewduino.Pages
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected ArduinoSerial mySerial = new ArduinoSerial();
+        protected BrewController BrewControl;
+        protected Dictionary<string, decimal> CurrentStatus;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BrewControl = new BrewController(mySerial);
+
+        }
+
+        protected void btnPreOpen_OnClick(object sender, EventArgs e)
+        {
+            SerialPort sp = new SerialPort();
+            sp.PortName = "/dev/ttyACM0";
+            sp.BaudRate = 9600;
+            sp.Parity = Parity.None;
+            sp.DataBits = 8;
+            sp.StopBits = StopBits.One;
+            sp.ReadTimeout = 5000;
+            sp.WriteTimeout = 500;
+
+            
+
+
+            lblTest.Text = SerialPort.GetPortNames()[0] +" Serial ports " + sp.PortName + sp.BaudRate + sp.StopBits + sp.DataBits;
+            
+        }
+
+        protected void btnTest_OnClick(object sender, EventArgs e)
+        {
+            SerialPort sp = new SerialPort();
+            sp.PortName = "/dev/ttyACM0";
+            sp.BaudRate = 9600;
+            sp.Parity = Parity.None;
+            sp.DataBits = 8;
+            sp.StopBits = StopBits.One;
+            sp.ReadTimeout = 5000;
+            sp.WriteTimeout = 500;
+
+            sp.Close();
+            sp.Open();
+
+
+            lblTest.Text = "We just opened";
+
+            sp.Close();
+        }
+    }
+}
